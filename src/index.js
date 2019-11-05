2a9f116025b7d71ad8e58c26a129b1ca2a509fb6
Tazhnae Brooks <tazhnae.brooks@salesforce.com>
2019-11-04 16:14:54 -0800

    noice..js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const Firstelement = <h6>cic calendar, NICE!</h6>;
+const Firstelement = <h6>cic calendar, NOICE!</h6>;
 
 ReactDOM.render(Firstelement, document.getElementById('root'));
